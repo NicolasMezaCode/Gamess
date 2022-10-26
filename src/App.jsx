696b062bb5ff82066ff611d7905ec922b84fbdab
Nicolas <nickismeza@gmail.com>
2022-10-25 22:44:28 -0700

    adding some assets

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+       <figure><img src="../src/assets/game-icons_game-console.png" alt="" /></figure>
     </div>
   )
 }
