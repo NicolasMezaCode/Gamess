6cdbdd94d643b2ea3e160caea2baa08e49f42891
Gabriel Soares de Brito <86431420+MendigoPower@users.noreply.github.com>
2022-10-27 02:15:14 -0700

    header and search with tailwind

--- src/components/Header/Navbar.components.jsx
+++ src/components/Header/Navbar.components.jsx
@@ -4,15 +4,15 @@ import './Navbar.styles.scss'
 
 export default function Navbar() {
   return (
-    <nav className='navbar'>
+    <nav className='flex bg-gray-300'>
         <div>
-          <h2>Gamess</h2>
           <img src={"./assets/game-icons_game-console.png"} />
+          <h2>Gamess</h2>
         </div>
         <Searchbar/>
-        <div className='navbar__buttons'>
-          <button>Log in</button>
-          <button>Register</button>
+        <div className='mt-3 mr-10 mb-3 ml-auto '>
+          <button className='mr-3.5 px-8 rounded-md bg-cyan-500'>Log in</button>
+          <button className='px-8 rounded-md bg-neutral-800 text-white'>Register</button>
         </div>
     </nav>
   )
--- src/components/Header/Searchbar.component.jsx
+++ src/components/Header/Searchbar.component.jsx
@@ -5,9 +5,9 @@ import './Searchbar.styles.scss';
 
 export default function Searchbar() {
   return (
-    <div className='searchbar'>
-        <input className='searchbar__input' type="text" placeholder='Search your game here' />
-        <figure><h3 className='searchbar__figure'><FiSearch/></h3></figure>
+    <div className='flex relative '>
+        <input className='w-96 rounded-md my-3' type="text" placeholder='Search your game here' />
+        <figure><h3 className='absolute my-4'><FiSearch/></h3></figure>
     </div>
   )
 }
