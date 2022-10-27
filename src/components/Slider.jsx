62d3185752d1b2349d425a8641a91e5e48e02dd6
Nicolas <nickismeza@gmail.com>
2022-10-26 23:29:28 -0700

    title created

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,11 +1,11 @@
 import React, { useState } from 'react'
 import readDead from '../assets/read-dead.png'
 import elden from '../assets/elden.webp'
-import gow from '../assets/gow.webp'
+import spider from '../assets/spiderman.jpg'
 import { useEffect } from 'react'
 export default function Slider() {
     const [counter,setCounter]=useState(0)
-    const images=[gow,elden,readDead]
+    const images=[readDead,elden,spider]
     const changeImage=()=>{
         if(counter<images.length-1){
             setCounter(prev=>prev+1)
@@ -21,8 +21,8 @@ export default function Slider() {
     // },[counter])
 
   return (
-    <div>
-        <figure className='flex justify-center items-center w-screen h-[28rem]  '><img className='w-screen h-full object-cover' src={images[counter]} alt="" /></figure>
-    </div>
+    <section className=''>
+        <figure className='flex justify-center items-center w-[100%] h-[29rem]  '><img className='z-10 w-[100%] h-full object-cover' src={images[counter]} alt="" /></figure>
+    </section>
   )
 }
