a96c575b3221f14fa3aef08ab21711a89536bb2e
Gabriel Soares de Brito <86431420+MendigoPower@users.noreply.github.com>
2022-10-28 16:59:56 -0700

    footer start

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,7 @@ import 'semantic-ui-css/semantic.min.css'
 import Navbar from "./components/Header/Navbar"
 import Footer from './components/Footer/Footer'
 import Slider from "./components/Slider.jsx"
-import Main from "./components/Main.jsx"
-
+import Main from './components/Main/Main';
 
 
 function App() {
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,11 +2,26 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <footer>
-            <div className='bg-black'>
-                <h2>Designed by: </h2>
-            </div>
-        </footer>
+      <footer class="mx-auto w-full max-w-container px-4 sm:px-6 lg:px-8">
+        <div class="border-t border-slate-900/5 py-10">
+          <svg  class="mx-auto h-5 w-auto text-slate-900"
+                aria-hidden="true"
+                viewBox="0 0 160 24"
+                fill="none">
+            <path class="fill-sky-400"></path>
+            <path fill="currentColor"></path>
+
+          </svg>
+          <p class="mt-5 text-center text-sm leading-6 text-slate-500">
+            Nicolas Meza Gordon Mathew Corp. All rights reserved.
+          </p>
+          <div class="mt-16 flex items-center justify-center space-x-4 text-sm font-semibold leading-6 text-slate-700">
+            <a href="">Instagram</a>
+            <div class="h-4 w-px bg-slate-500/20"></div>
+            <a href="">Github</a>
+          </div>
+        </div>
+      </footer>
     );
 };
 
