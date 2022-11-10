8dc2f4a66c55eec66949da65d08e19f4d0ec1e4b
Gabriel Soares de Brito <86431420+MendigoPower@users.noreply.github.com>
2022-11-10 09:26:05 -0800

    small design changes

--- src/components/GamePage/Description.jsx
+++ src/components/GamePage/Description.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 export default function Description() {
 
   return (
-    <div className='h-96'>
-        <div className=''>
+    <div className='h-128'>
+        <div className='pt-80'>
         <h2 className=''>God of War II continues the story of Kratos, with him wielding his newly-found godlike powers,
          much to the dismay and fear of the other gods.</h2>
         </div>
--- src/components/GamePage/GameHeader.jsx
+++ src/components/GamePage/GameHeader.jsx
@@ -11,14 +11,13 @@ export default function GameHeader() {
                 <div className='h-140 w-screen'>
                     <figure style={{ backgroundImage: `url(${GoW})` }} className='bg-no-repeat bg-cover bg-center blur-md min-h-full min-w-full bg-fixed'></figure>
                 </div>
-
             <div className='absolute min-w-full pt-40' >
                 <div className='grid grid-cols-2'>
                     <div className='ml-80'>
                         <img src={GoW} alt="" className=' rounded-lg w-128 h-132 border-solid border-2 border-black' />
                     </div>
                     <div className='backdrop-blur-xl bg-white/80 w-140  rounded-lg'>
-                        <div className='grid grid-cols-2 min-h-full p-5'>
+                        <div className='grid grid-cols-2 col-span-2 min-h-full p-5'>
                             <div className=''>
                                 <ul className=''>
                                     <li>Genre: </li>
@@ -28,8 +27,8 @@ export default function GameHeader() {
                                 </ul>
                             </div>
                             <div>
-                                <ul className=''>
-                                    <li>test </li>
+                                <ul className='col-span-1'>
+                                    <li>test Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis reprehenderit, aspernatur mollitia suscipit, asperiores nostrum veniam pariatur officia impedit unde vel repellat fuga neque? Inventore saepe tenetur mollitia ipsam molestiae! </li>
                                     <li>test </li>
                                     <li>test </li>
                                     <li>test </li>
