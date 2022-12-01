1946408827a8e0fd71e274d34e2243e5b9990a93
Nicolas <nickismeza@gmail.com>
2022-11-30 20:40:16 -0800

    change in screenshot

--- Client/src/components/GamePage/GamePage.tsx
+++ Client/src/components/GamePage/GamePage.tsx
@@ -7,6 +7,7 @@ import Information from './Information'
 import Sellers from './Sellers'
 import { useParams } from "react-router-dom";
 import {getGamePage} from "../../helpers/getGamePage"
+import { use } from "express/lib/router";
 
 export default function Game() {
   // How to get the id from the parameters
@@ -21,7 +22,17 @@ export default function Game() {
       setGameData(data[0])
     })
   }, []);
-  
+  useEffect(()=>{
+    let imageUrls=[]
+    if(gameData!=undefined){ 
+      gameData.screenshots.map((screenshot)=>{
+        const id=screenshot.image_id
+        imageUrls.push(`https://images.igdb.com/igdb/image/upload/t_1080p/${id}.jpg`)
+      })
+    }
+    console.log(imageUrls)
+  },[gameData])
+
   return (
     <div className=''>
       {gameData?<Hero name={gameData.name} cover={gameData.cover} genres={gameData.genres} platforms={gameData.platforms} rating={gameData.aggregated_rating} engine={gameData.game_engines}  />:null
