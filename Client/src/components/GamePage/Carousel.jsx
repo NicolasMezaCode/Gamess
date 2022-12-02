ad1db2cab0c4157cc011c84168979dc5367a8711
Gabriel Soares de Brito <86431420+MendigoPower@users.noreply.github.com>
2022-12-02 01:05:28 -0800

    updates

--- Client/src/components/GamePage/Carousel.jsx
+++ Client/src/components/GamePage/Carousel.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
-export default function Gallery({screenshot}) {
-
+export default function Gallery(screenshot) {
+    const screenshots=`https://images.igdb.com/igdb/image/upload/t_cover_big/${screenshot.image_id}.jpg`
   return (
     <Carousel autoPlay interval="800000" transitionTime="1000" infiniteLoop>
     <div>
-        <img src={"https://images.igdb.com/igdb/image/upload/t_1080p/scfvki.jpg"} alt="" />
+        <img src={screenshots} alt="" />
     </div>
     {/* <div>
         <img src="https://images.igdb.com/igdb/image/upload/t_1080p/x7pzfljardlljvtqcgv4.jpg" alt="" />
