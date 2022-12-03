43eb0c949bdd1aab1b13fcf11cb4b01929a74ae1
Gabriel Soares de Brito <86431420+MendigoPower@users.noreply.github.com>
2022-12-03 01:05:47 -0800

    minor changes

--- Client/src/components/GamePage/Similar.jsx
+++ Client/src/components/GamePage/Similar.jsx
@@ -3,14 +3,14 @@ import React from "react";
 export default function Similar({name, cover, id}) {
     const image=`https://images.igdb.com/igdb/image/upload/t_cover_big/${cover.image_id}.jpg`
     return (
-            <div className="py-10 max-w-screen-xl mx-auto rounded-md bg-medium-grey shadow-xl hover:drop-shadow-lg transition-all duration-300 ease-linear min-h-fit ">
+            <div className="py-10 max-w-screen-xl mx-auto rounded-md bg-medium-grey shadow-xl hover:drop-shadow-lg transition-all duration-300 ease-linear min-h-fit mt-7 mb-24">
                 <h3 className="p-5 text-center text-white font-poppins mr-1 text-3xl">Similar Games</h3>
-                <div className="grid grid-col-4 grid-flow-col gap-4">
-                    <div className="mt-3">
+                <div className="grid grid-col-4 grid-flow-col gap-4 ">
+                    <div className="mt-3 ">
                         <a to={`/game/${id}`}>
-                            <figure className="ml-9 mr-9">
-                                <img src={image} alt="cover of the games"></img>
-                                <span className="flex  items-center justify-center mt-2">
+                            <figure className="ml-9 mr-9 hover:drop-shadow-lg transition-all duration-300 ease-linear ">
+                                <img src={image} alt="cover of the games" className=""></img>
+                                <span className="flex  items-center justify-center mt-2 ">
                                     <h4 className="text-white font-poppins mr-1 text-3xl">{name}</h4>
                                 </span>
                             </figure>
@@ -19,7 +19,7 @@ export default function Similar({name, cover, id}) {
                     <div>
                         <div className="mt-3">
                             <a to={`/game/${id}`}>
-                                <figure className="ml-9 mr-9">
+                                <figure className="ml-9 mr-9 hover:drop-shadow-lg transition-all duration-300 ease-linear">
                                     <img src={image} alt="cover of the games"></img>
                                     <span className="flex  items-center justify-center mt-2">
                                         <h4 className="text-white font-poppins mr-1 text-3xl">{name}</h4>
@@ -31,7 +31,7 @@ export default function Similar({name, cover, id}) {
                     <div>
                         <div className="mt-3">
                             <a to={`/game/${id}`}>
-                                <figure className="ml-9 mr-9">
+                                <figure className="ml-9 mr-9 hover:drop-shadow-lg transition-all duration-300 ease-linear">
                                     <img src={image} alt="cover of the games"></img>
                                     <span className="flex  items-center justify-center mt-2">
                                         <h4 className="text-white font-poppins mr-1 text-3xl">{name}</h4>
@@ -43,7 +43,7 @@ export default function Similar({name, cover, id}) {
                     <div>
                         <div className="mt-3">
                             <a hto={`/game/${id}`}>
-                                <figure className="ml-9 mr-9">
+                                <figure className="ml-9 mr-9 hover:drop-shadow-lg transition-all duration-300 ease-linear">
                                     <img src={image} alt="cover of the games"></img>
                                     <span className="flex  items-center justify-center mt-2">
                                         <h4 className="text-white font-poppins mr-1 text-3xl">{name}</h4>
