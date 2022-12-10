b8dc60519a67f7fbbc0b3669f4ea08bf9f93297a
Gabriel Soares de Brito <86431420+MendigoPower@users.noreply.github.com>
2022-12-10 13:37:10 -0800

    media querys

--- Client/src/components/GamePage/Hero.jsx
+++ Client/src/components/GamePage/Hero.jsx
@@ -11,12 +11,12 @@ export default function Hero({ cover, rating, name, genres, platforms, engine })
             <div className='h-140 w-screen'>
                 <figure style={{ backgroundImage: `url(${image})` }} className='bg-no-repeat bg-cover bg-center blur-md min-h-full min-w-full bg-fixed '></figure>
             </div>
-            <div className='absolute min-w-full pt-40' >
-                <div className='grid grid-cols-2'>
-                    <div className='ml-80'>
-                        <img src={imageSmall} alt="" className=' rounded-xl w-128 h-132 border-solid border-4 border-black shadow-xl hover:drop-shadow-lg transition-all duration-300 ease-linear ' />
+            <div className='absolute min-w-full pt-40 p-16' >
+                <div className='lg:grid grid-cols-2 lg:gap-10'>
+                    <div className='mx-auto w-128 h-132  lg:mb-0 mb-10'>
+                        <img src={imageSmall} alt="" className=' w-screen  rounded-xl border-solid border-4 border-black shadow-xl hover:drop-shadow-lg transition-all duration-300 ease-linear ' />
                     </div>
-                    <div className='backdrop-blur-xl bg-white/80 w-140  rounded-xl shadow-xl hover:drop-shadow-lg transition-all duration-300 ease-linear '>
+                    <div className='mx-auto xl:w-140 backdrop-blur-xl bg-white/80 w-128 rounded-xl shadow-xl hover:drop-shadow-lg transition-all duration-300 ease-linear '>
                         <div className='text-center'>
                             <h2 className='py-12 text-5xl'>{name}</h2>
                             <span className='flex  items-center justify-center mt-3 mb-3'>
@@ -24,7 +24,7 @@ export default function Hero({ cover, rating, name, genres, platforms, engine })
                                 <AiFillStar className='text-amber-600 w-7 h-auto' />
                             </span>
                         </div>
-                        <div className=' px-36 text-2xl'>
+                        <div className=' xl:px-36 px-16 text-2xl'>
                             <ul className='grid grid-cols-2 min-h-full'>
                                 <li className='py-10'>Genres: </li>
                                 <li className='py-10'>{genres ? genres.map((genre) => <li className='ml-1 mr-1' key={genre.id}>{genre.name}</li>) : null}</li>
