08b27b3b0d9893078bd2bc79550fb2f4509407fd
Gabriel Soares de Brito <86431420+MendigoPower@users.noreply.github.com>
2022-12-13 22:26:01 -0800

    like button on gamepage

--- Client/src/components/GamePage/Hero.jsx
+++ Client/src/components/GamePage/Hero.jsx
@@ -1,22 +1,30 @@
-import { React } from 'react'
-import { AiFillStar } from 'react-icons/Ai'
+import { React, useState } from 'react'
+import { AiFillStar, AiFillHeart } from 'react-icons/Ai'
 
 export default function Hero({ cover, rating, name, genres, platforms, engine }) {
     const imageSmall = `https://images.igdb.com/igdb/image/upload/t_cover_big/${cover.image_id}.jpg`
     const image = `https://images.igdb.com/igdb/image/upload/t_1080p/${cover.image_id}.jpg`
     const stars = Math.round(rating) * 5 / 100
     const formatedRating = stars.toFixed(1)
+    const [likeColor, setLikeColor] = useState('grey');
     return (
         <div className='flex min-h-fit relative'>
             <div className='h-140 w-screen min-w-full  '>
                 <figure style={{ backgroundImage: `url(${image})` }} className='bg-no-repeat bg-cover bg-center blur-md min-h-full min-w-full bg-fixed '></figure>
             </div>
             <div className='absolute min-w-full  mobile:p-16 p:7 mt-16' >
+
                 <div className='laptop:grid grid-cols-2 laptop:gap-10'>
+
                     <div className='mx-auto tablet:w-128 mobile:w-96 w-72  h-132  laptop:mb-0 mb-10 '>
                         <img src={imageSmall} alt="" className='mx-auto w-screen  rounded-xl border-solid border-4 border-black shadow-xl hover:drop-shadow-lg transition-all duration-300 ease-linear ' />
                     </div>
                     <div className='mx-auto xl:w-140 backdrop-blur-xl bg-white/80 mobile:w-128 w-72 rounded-xl shadow-xl hover:drop-shadow-lg transition-all duration-300 ease-linear '>
+                        <div className='absolute w-full h-full '>
+                            <div className='flex w-full h-full place-content-end items-end opacity:50 text-'>
+                                <AiFillHeart onClick={() => setLikeColor('red')} color={likeColor} className='w-10 h-8 m-5 hover:text-red transition-all duration-300 ease-linear' />
+                            </ div>
+                        </div>
                         <div className='text-center'>
                             <h2 className='mobile:py-12 pt-7 pb-2 mobile:text-5xl text-3xl'>{name}</h2>
                             <span className='flex  items-center justify-center mt-3 mb-3'>
@@ -24,6 +32,7 @@ export default function Hero({ cover, rating, name, genres, platforms, engine })
                                 <AiFillStar className='text-amber-600 w-7 h-auto' />
                             </span>
                         </div>
+
                         <div className=' xl:px-36 px-16 mobile:text-2xl text-xl'>
                             <ul className='grid grid-cols-2 min-h-full'>
                                 <li className='mobile:py-10 py-2 mobile:ml-0 -ml-10 mobile:no-underline overline'>Genres: </li>
@@ -41,7 +50,7 @@ export default function Hero({ cover, rating, name, genres, platforms, engine })
                         </div>
                     </div>
                 </div>
-                
+
             </div>
         </div>
     )
