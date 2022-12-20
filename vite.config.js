b705015443f593cb5fd78e96e5e8dd5b18751301
Nicolas <nickismeza@gmail.com>
2022-12-20 14:00:57 -0800

    fix

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: { port: 4000 },
+export default defineConfig({    
+  plugins: [react({
+    input: './src/main.jsx',
+  })],
+  server: { port: 3000 },
   build: {
     outDir: 'dist',
     emptyOutDir: true,
