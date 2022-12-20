4f830fb079ab5bf8029a3102560d03ca2ae21e2d
Nicolas <nickismeza@gmail.com>
2022-12-20 13:01:28 -0800

    change in vite config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: { port: 3000 },
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      external: ['react', 'react-dom', 'react-router-dom', 'react-icons'],
+    },
+  },
 })
