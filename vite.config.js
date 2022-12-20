0a7d49f52316a50489f16862d7dd751ab7a7b7ae
Nicolas <nickismeza@gmail.com>
2022-12-20 13:51:02 -0800

    imports

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,19 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: { port: 3000 },
+  server: { port: 4000 },
   build: {
     outDir: 'dist',
     emptyOutDir: true,
     rollupOptions: {
-      //add react-icons to external
       external: ['react-icons/Ai','react-icons/Bs' ,'react-router-dom', 'react-router', 'react', 'react-dom'],
+      output: {
+        globals:{
+          react: 'React',
+          'react-router-dom': 'ReactRouterDOM',
+          'react-router': 'ReactRouter',
+          'react-dom': 'ReactDOM'
+        }},
     },
   },
 })
