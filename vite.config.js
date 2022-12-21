4dd251a13290036f93c03713fc95c00abab44232
Nicolas <nickismeza@gmail.com>
2022-12-21 00:17:53 -0800

    yep

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import reactRouter from 'vite-plugin-react-router'
 
 // https://vitejs.dev/config/
 export default defineConfig({
