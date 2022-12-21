cbdbb665b48c1baa42d901423bd879af0c40ab61
Nicolas <nickismeza@gmail.com>
2022-12-21 00:10:56 -0800

    i do now

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,18 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      'react': 'https://cdn.skypack.dev/react@17',
+      'react-dom': 'https://cdn.skypack.dev/react-dom@17',
+      'react-router-dom': 'https://cdn.skypack.dev/react-router-dom@5',
+      'react-icons': 'https://cdn.skypack.dev/react-icons@4',
+    }
+  },
   plugins: [react()],
   server: { port: 3000 },
   build: {
     outDir: 'dist',
     emptyOutDir: true,
-    rollupOptions: {
-      external: ['react-icons/Ai','react-icons/Bs' ,'./react-router-dom', './react-router']
-    }
   },
 })
